46dd6f9fe93a038ac1032f29f201c06a7db63896
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2025-02-09 03:44:20 -0500

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,43 +1,20 @@
 resource "aws_lambda_function" "job_application_lambda" {
   function_name    = "job-application-lambda"
   role            = aws_iam_role.job_application_lambda_role.arn
-  handler         = "lambda_function.lambda_handler"
   runtime         = "python3.9"
+  handler         = "apply_jobs.lambda_handler"
   filename        = "lambda_package.zip"
   source_code_hash = filebase64sha256("lambda_package.zip")
+  timeout         = 10
 
   environment {
     variables = {
-      ENV = "Production"
+      DYNAMO_TABLE_NAME = aws_dynamodb_table.job_applications.name
     }
-  }
-}
-
-resource "aws_iam_role" "job_application_lambda_role" {
-  name               = "job-application-lambda-role"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-
-  lifecycle {
-    ignore_changes = [assume_role_policy]
   }
 
   tags = {
-    Name        = "Job Application Lambda Role"
+    Name        = "Job Application Lambda"
     Environment = "Production"
   }
-}
-
-resource "aws_lambda_permission" "allow_s3_trigger" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.job_application_lambda.function_name
-  principal     = "s3.amazonaws.com"
 }
