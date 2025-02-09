18b03619bd02f1bfd2ff295fd090bf38d1f7a7fc
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2025-02-09 01:46:04 -0500

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,16 +1,81 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "job_application_lambda_role" {
+  name = "job-application-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "job-application-lambda-policy"
+  description = "IAM policy for Lambda function to access S3, SNS, and Logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "sns:Publish",
+          "sns:Subscribe"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.job_application_lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
 resource "aws_lambda_function" "job_application_lambda" {
-  function_name = "ApplyJobsLambda"
-  role          = aws_iam_role.lambda_role.arn  # Now correctly referencing the IAM role
-  runtime       = "python3.8"
-  handler       = "lambda_function.lambda_handler"
-  
-  filename         = "lambda_package.zip"
-  source_code_hash = filebase64sha256("lambda_package.zip")
+  function_name    = "ApplyJobsLambda"
+  role            = aws_iam_role.job_application_lambda_role.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.8"
+  memory_size     = 128
+  timeout         = 3
+  package_type    = "Zip"
+  filename        = "lambda_package.zip"  # Ensure this file exists before running Terraform
 
   environment {
     variables = {
-      AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
-      AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+      LOG_LEVEL = "INFO" # Example variable; you can add more
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_policy_attachment
+  ]
 }
