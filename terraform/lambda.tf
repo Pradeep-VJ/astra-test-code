df39e342b6bd1fa0a9a0d6178dd07ad28618ee2f
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2025-02-08 22:45:41 -0500

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,16 @@
 resource "aws_lambda_function" "job_application_lambda" {
   function_name = "ApplyJobsLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "python3.9"
-  handler       = "apply_jobs.lambda_handler"
-  filename      = "../lambda/build.zip"
-  memory_size   = 3072
-  timeout       = 900
+  role          = aws_iam_role.lambda_role.arn  # Now correctly referencing the IAM role
+  runtime       = "python3.8"
+  handler       = "lambda_function.lambda_handler"
+  
+  filename         = "lambda_package.zip"
+  source_code_hash = filebase64sha256("lambda_package.zip")
+
+  environment {
+    variables = {
+      AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
+      AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+    }
+  }
 }
