72016a0b18d675d2a1143cc3547722419a1a0169
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2025-02-08 23:42:38 -0500

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,15 +1,23 @@
 resource "aws_dynamodb_table" "job_applications" {
-  name           = "JobApplications"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "job_id"
+  name         = "JobApplications"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "job_id"  # This is the partition key
 
   attribute {
     name = "job_id"
     type = "S"
   }
 
+  # If "student" is required, it should be a sort key or an indexed attribute
   attribute {
     name = "student"
     type = "S"
+  }
+
+  # Optional: Add a global secondary index for student attribute if required
+  global_secondary_index {
+    name            = "StudentIndex"
+    hash_key        = "student"
+    projection_type = "ALL"
   }
 }
