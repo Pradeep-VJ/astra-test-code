efee5fdf8a43545ac482f76c06c87abe2d1842e1
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2025-02-08 23:39:25 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,14 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_REGION" {
+  description = "AWS region where resources will be created"
+  default     = "us-east-1" # Change this if needed
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Key"
+  type        = string
+}
